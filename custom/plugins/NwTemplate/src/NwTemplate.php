13d5416c828be640a74a0edeb4eae8af273f8a4e
Christoph <support@netwerk.at>
2025-09-10 16:04:12 +0200

    Install NwTemplate [cr]

---
+++ custom/plugins/NwTemplate/src/NwTemplate.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace NwTemplate;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class NwTemplate extends Plugin implements ThemeInterface
+{
+}
